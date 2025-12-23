fdd48495fb8870a3826284245e85bcd14a28fcb5
Yibo Cao <cyb@ieee.org>
2025-12-23 08:58:14 -0500

    almost done

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod format;
 
-use eframe::egui::{CentralPanel, Context, Key, Modal, Pos2, TextEdit, ThemePreference, Ui};
-use eframe::{CreationContext, Frame, NativeOptions, run_native};
-use egui_snarl::ui::{PinInfo, SnarlPin, SnarlStyle, SnarlViewer};
+use core::f32;
+use eframe::egui::{Align, CentralPanel, Color32, Context, Frame, Key, Layout, Modal, Pos2, RichText, TextEdit, ThemePreference, Ui, Vec2, vec2};
+use eframe::{CreationContext, NativeOptions, run_native};
+use egui_snarl::ui::{PinInfo, PinPlacement, SnarlPin, SnarlStyle, SnarlViewer};
 use egui_snarl::{InPin, InPinId, NodeId, OutPin, OutPinId, Snarl};
 use meval::eval_str;
 use std::{collections::HashMap, mem::take};
@@ -71,8 +72,17 @@ impl ChartStats {
 /// Return whether to retain.
 type ModalBox = Box<dyn FnMut(&mut App, &mut Ui) -> bool>;
 
+enum DeferredAction {
+    None,
+    AddConsume(NodeId),
+    AddProduce(NodeId),
+    RemoveConsume(InPinId),
+    RemoveProduce(OutPinId),
+}
+
 struct ChartViewer<'a> {
     modal: &'a mut Option<ModalBox>,
+    action: DeferredAction,
     stats: ChartStats,
 }
 
@@ -89,6 +99,12 @@ fn make_input_text_modal(prompt: &'static str, submit: impl Fn(&mut App, String)
         }
         !ui.input(|x| x.key_pressed(Key::Escape))
     })
+}
+
+fn prepare_small_button(ui: &mut Ui) {
+    let spacing = &mut ui.style_mut().spacing;
+    spacing.button_padding = Vec2::ZERO;
+    spacing.item_spacing = vec2(1., 0.);
 }
 
 impl SnarlViewer<NodeMeta> for ChartViewer<'_> {
@@ -114,28 +130,58 @@ impl SnarlViewer<NodeMeta> for ChartViewer<'_> {
     }
 
     fn show_header(&mut self, node: NodeId, _: &[InPin], _: &[OutPin], ui: &mut Ui, chart: &mut Snarl<NodeMeta>) {
-        let label = match &mut chart[node] {
-            NodeMeta::Resource(label) => label,
-            NodeMeta::Process(meta) => &mut meta.label,
+        let (width, label) = match &mut chart[node] {
+            NodeMeta::Resource(label) => (80., label),
+            NodeMeta::Process(meta) => {
+                let mut width = 80.;
+                (!meta.consumes.is_empty()).then(|| width += 50.);
+                (!meta.produces.is_empty()).then(|| width += 50.);
+                (width, &mut meta.label)
+            }
         };
-        TextEdit::singleline(label).desired_width(80.).show(ui);
+        ui.set_width(width);
+        TextEdit::singleline(label).desired_width(f32::INFINITY).show(ui);
+    }
+
+    fn node_frame(&mut self, mut frame: Frame, node: NodeId, _: &[InPin], _: &[OutPin], _: &Snarl<NodeMeta>) -> Frame {
+        let Some(stats) = self.stats.nodes.get(&node) else { return frame };
+        match stats {
+            NodeStats::Process(valid) => _ = (!valid).then(|| frame.fill = Color32::DARK_RED),
+            NodeStats::Resource(rate) => {
+                if *rate < -1E-20 {
+                    frame.fill = Color32::from_rgb(160, 80, 0);
+                } else if *rate > 1E-20 {
+                    frame.fill = Color32::DARK_GREEN;
+                }
+            }
+        }
+        frame
     }
 
     fn has_body(&mut self, _: &NodeMeta) -> bool { true }
-    fn show_body(&mut self, node: NodeId, inputs: &[InPin], outputs: &[OutPin], ui: &mut Ui, chart: &mut Snarl<NodeMeta>) {
+    fn show_body(&mut self, node: NodeId, _: &[InPin], _: &[OutPin], ui: &mut Ui, chart: &mut Snarl<NodeMeta>) {
         match &mut chart[node] {
             NodeMeta::Resource(_) => {
-                ui.label(format_float(self.stats.resource_rate(node)));
+                ui.set_width(72.);
+                ui.vertical_centered(|ui| ui.label(format_float(self.stats.resource_rate(node))));
             }
             NodeMeta::Process(meta) => {
+                ui.set_width(100.);
                 ui.vertical(|ui| {
                     ui.horizontal(|ui| {
-                        ui.label("Count: ");
-                        TextEdit::singleline(&mut meta.count).desired_width(50.).show(ui);
+                        ui.label("Qty");
+                        TextEdit::singleline(&mut meta.count).desired_width(f32::INFINITY).show(ui);
                     });
                     ui.horizontal(|ui| {
-                        ui.label("Speed: ");
-                        TextEdit::singleline(&mut meta.speed).desired_width(50.).show(ui);
+                        ui.label("Spd");
+                        TextEdit::singleline(&mut meta.speed).desired_width(f32::INFINITY).show(ui);
+                    });
+                    ui.horizontal(|ui| {
+                        prepare_small_button(ui);
+                        ui.small_button("➕").clicked().then(|| self.action = DeferredAction::AddConsume(node));
+                        ui.with_layout(Layout::right_to_left(Align::Center), |ui| {
+                            ui.small_button("➕").clicked().then(|| self.action = DeferredAction::AddProduce(node));
+                        });
                     });
                 });
             }
@@ -151,7 +197,14 @@ impl SnarlViewer<NodeMeta> for ChartViewer<'_> {
 
     fn show_input(&mut self, pin: &InPin, ui: &mut Ui, chart: &mut Snarl<NodeMeta>) -> impl SnarlPin + 'static {
         if let NodeMeta::Process(meta) = &mut chart[pin.id.node] {
-            TextEdit::singleline(&mut meta.consumes[pin.id.input]).desired_width(20.).show(ui);
+            ui.vertical(|ui| {
+                TextEdit::singleline(&mut meta.consumes[pin.id.input]).desired_width(20.).show(ui);
+                ui.horizontal(|ui| {
+                    prepare_small_button(ui);
+                    ui.small_button("✖").clicked().then(|| self.action = DeferredAction::RemoveConsume(pin.id));
+                    ui.small_button("➡");
+                });
+            });
         }
         PinInfo::square()
     }
@@ -165,7 +218,15 @@ impl SnarlViewer<NodeMeta> for ChartViewer<'_> {
 
     fn show_output(&mut self, pin: &OutPin, ui: &mut Ui, chart: &mut Snarl<NodeMeta>) -> impl SnarlPin + 'static {
         if let NodeMeta::Process(meta) = &mut chart[pin.id.node] {
-            TextEdit::singleline(&mut meta.produces[pin.id.output]).desired_width(20.).show(ui);
+            ui.set_width(30.);
+            ui.vertical(|ui| {
+                TextEdit::singleline(&mut meta.produces[pin.id.output]).desired_width(20.).show(ui);
+                ui.horizontal(|ui| {
+                    prepare_small_button(ui);
+                    ui.small_button("⬅");
+                    ui.small_button("✖").clicked().then(|| self.action = DeferredAction::RemoveProduce(pin.id));
+                });
+            });
         }
         PinInfo::square()
     }
@@ -184,6 +245,11 @@ impl SnarlViewer<NodeMeta> for ChartViewer<'_> {
             chart.insert_node(pos, NodeMeta::Process(meta));
         });
     }
+
+    fn has_node_menu(&mut self, _: &NodeMeta) -> bool { true }
+    fn show_node_menu(&mut self, node: NodeId, inputs: &[InPin], outputs: &[OutPin], ui: &mut Ui, chart: &mut Snarl<NodeMeta>) {
+        ui.button("Delete").clicked().then(|| chart.remove_node(node));
+    }
 }
 
 struct App {
@@ -193,10 +259,44 @@ struct App {
 }
 
 impl eframe::App for App {
-    fn update(&mut self, ctx: &Context, _: &mut Frame) {
+    fn update(&mut self, ctx: &Context, _: &mut eframe::Frame) {
         CentralPanel::default().show(ctx, |ui| {
             let stats = ChartStats::compute(&self.chart);
-            self.chart.show(&mut ChartViewer { modal: &mut self.modal, stats }, &self.style, (), ui);
+            let mut viewer = ChartViewer { modal: &mut self.modal, action: DeferredAction::None, stats };
+            self.chart.show(&mut viewer, &self.style, (), ui);
+            match viewer.action {
+                DeferredAction::None => (),
+                DeferredAction::AddConsume(node) => {
+                    let NodeMeta::Process(meta) = &mut self.chart[node] else { unreachable!() };
+                    meta.consumes.push("1".to_owned());
+                }
+                DeferredAction::AddProduce(node) => {
+                    let NodeMeta::Process(meta) = &mut self.chart[node] else { unreachable!() };
+                    meta.produces.push("1".to_owned());
+                }
+                DeferredAction::RemoveConsume(pin) => {
+                    let NodeMeta::Process(meta) = &mut self.chart[pin.node] else { unreachable!() };
+                    let old_len = meta.consumes.len();
+                    meta.consumes.remove(pin.input);
+                    self.chart.drop_inputs(pin);
+                    for i in pin.input + 1..old_len {
+                        let old = InPinId { node: pin.node, input: i };
+                        let new = InPinId { node: pin.node, input: i - 1 };
+                        self.chart.in_pin(old).remotes.into_iter().for_each(|far| _ = self.chart.connect(far, new));
+                    }
+                }
+                DeferredAction::RemoveProduce(pin) => {
+                    let NodeMeta::Process(meta) = &mut self.chart[pin.node] else { unreachable!() };
+                    let old_len = meta.produces.len();
+                    meta.produces.remove(pin.output);
+                    self.chart.drop_outputs(pin);
+                    for i in pin.output + 1..old_len {
+                        let old = OutPinId { node: pin.node, output: i };
+                        let new = OutPinId { node: pin.node, output: i - 1 };
+                        self.chart.out_pin(old).remotes.into_iter().for_each(|far| _ = self.chart.connect(new, far));
+                    }
+                }
+            }
         });
         if let Some(mut modal) = self.modal.take() {
             Modal::new("modal".into()).show(ctx, |ui| {
@@ -208,7 +308,14 @@ impl eframe::App for App {
 
 fn make_app(cc: &CreationContext) -> App {
     cc.egui_ctx.set_theme(ThemePreference::Dark);
-    App { style: SnarlStyle::new(), chart: Snarl::new(), modal: None }
+    let style = SnarlStyle {
+        header_drag_space: Some(Vec2::ZERO),
+        collapsible: Some(false),
+        wire_width: Some(3.),
+        pin_placement: Some(PinPlacement::Edge),
+        ..<_>::default()
+    };
+    App { style, chart: Snarl::new(), modal: None }
 }
 
 fn main() {
